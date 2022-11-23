cc2875ff381dd72ee0671c972a93f788b7d39df3
NinnjA254 <quaypeter@yahoo.com>
2022-11-23 16:17:19 +0300

    enable loading the program from a file instead of a line by line user prompt

--- load_program.c
+++ load_program.c
@@ -1,21 +1,42 @@
 #include "main.h"
+#define SENTINEL -99999/* When the the program_loader reads this value it will stop reading instructions into memory.*/
+
 void load_program(char *program_file, Cpu *cpu)
 {
     int i;
     int read;
-    for (i = 0; i < 100; i++)
-    { // load program
-        printf("%02d ? ", i);
-        scanf("%d", &read);
-        if (read == -99999)
+	int sentinel_found = 0; /*used as a bool*/
+	FILE *fptr;
+
+	fptr = fopen(program_file, "r");
+
+	for (i =0; i < 100; i++)
+	{ // load program
+        fscanf(fptr, "%d", &read);
+        if (read == SENTINEL)
+		{
+			sentinel_found = 1;
             break;
+		}
 
 		if (read < -9999 || read > 9999)
 		{
-			printf("Invalid input: input must be between 9999 and  -9999\n", i);
-			printf("%02d ? ", i);
-			scanf("%d", &read);
+			printf("load ERROR: line %d , Invalid input: input must be between 9999 and  -9999\n", i);
+			fclose(fptr);
+			exit(-1);
+		}
+		if (read == EOF)
+		{
+
 		}
         cpu->memory[i] = read;
-    }
+	}
+	if (!sentinel_found)
+	{
+			printf("ERROR: Reached end of file without encountering the sentinel -99999\n", i);
+			fclose(fptr);
+			exit(-1);
+	}
+	fclose(fptr);
+	
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 
 	if (argc  != 2)
 	{
-		printf("usage, %s SML program file name\n", argc[0]);
+		printf("usage, %s SML program file name\n", argv[0]);
 		return (-1);
 	}
     init(&cpu);
